654a17f1bcd0a37fd8a9b67fd15408637b00880b
golamrabbani93 <golamrabbani9373@gmail.com>
2023-08-27 21:53:25 +0600

    setup navbar and styling

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Navbar = () => {
+	const navlink = (
+		<>
+			<li>
+				<Link className="active" to={'/'} end>
+					Home
+				</Link>
+			</li>
+			<li>
+				<Link to={'/about'}>About</Link>
+			</li>
+			<li>
+				<Link to={'/appointment'}>Appointment</Link>
+			</li>
+			<li>
+				<Link to={'/login'}>Login</Link>
+			</li>
+		</>
+	);
 	return (
-		<div className="navbar bg-base-100">
+		<div className="navbar bg-base-100 flex justify-between m-auto w-[1440px]">
 			<div className="navbar-start">
 				<div className="dropdown">
 					<label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -25,52 +44,13 @@ const Navbar = () => {
 						tabIndex={0}
 						className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
 					>
-						<li>
-							<a>Item 1</a>
-						</li>
-						<li>
-							<a>Parent</a>
-							<ul className="p-2">
-								<li>
-									<a>Submenu 1</a>
-								</li>
-								<li>
-									<a>Submenu 2</a>
-								</li>
-							</ul>
-						</li>
-						<li>
-							<a>Item 3</a>
-						</li>
+						{navlink}
 					</ul>
 				</div>
-				<a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+				<Link className="btn btn-ghost normal-case text-xl">Medicational</Link>
 			</div>
 			<div className="navbar-center hidden lg:flex">
-				<ul className="menu menu-horizontal px-1">
-					<li>
-						<a>Item 1</a>
-					</li>
-					<li tabIndex={0}>
-						<details>
-							<summary>Parent</summary>
-							<ul className="p-2">
-								<li>
-									<a>Submenu 1</a>
-								</li>
-								<li>
-									<a>Submenu 2</a>
-								</li>
-							</ul>
-						</details>
-					</li>
-					<li>
-						<a>Item 3</a>
-					</li>
-				</ul>
-			</div>
-			<div className="navbar-end">
-				<a className="btn">Button</a>
+				<ul className="menu menu-horizontal px-1">{navlink}</ul>
 			</div>
 		</div>
 	);

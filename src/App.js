8806dd954d73e8fe7ef50ebd0d152dd34051f632
golamrabbani93 <golamrabbani9373@gmail.com>
2023-08-27 21:42:12 +0600

    create some components and setup routes  and router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
+import {RouterProvider} from 'react-router-dom';
 import './App.css';
+import router from './Routes/Routes/Routes';
 
 function App() {
 	return (
 		<div className="App">
-			<button className="btn">Button</button>
-			<button className="btn btn-neutral">Neutral</button>
-			<button className="btn btn-primary">Primary</button>
-			<button className="btn btn-secondary">Secondary</button>
-			<button className="btn btn-accent">Accent</button>
-			<button className="btn btn-ghost">Ghost</button>
-			<button className="btn btn-link">Link</button>
+			<RouterProvider router={router} />
 		</div>
 	);
 }

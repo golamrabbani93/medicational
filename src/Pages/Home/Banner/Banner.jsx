bd293d493230fbe6e9c25bda678582ac6c81281e
golamrabbani93 <golamrabbani9373@gmail.com>
2023-08-27 22:26:04 +0600

    Banner complete

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import chair from '../../../assets/images/chair.png';
+import bgchair from '../../../assets/images/bg.png';
+
+const Banner = () => {
+	return (
+		<div
+			className="hero min-h-screen w-[1363px] m-auto "
+			style={{
+				backgroundImage: `url(${bgchair})`,
+			}}
+		>
+			<div className="hero-content flex-col lg:flex-row-reverse">
+				<img src={chair} className=" max-w-xl rounded-lg shadow-2xl" alt="" />
+				<div>
+					<h1 className="text-5xl font-bold">
+						Your New Smile Starts <br /> Here
+					</h1>
+					<p className="py-6">
+						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+						has been the industry's standard dummy text ever since the
+					</p>
+					<button className="btn btn-primary">Get Started</button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Banner;

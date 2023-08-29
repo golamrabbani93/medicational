6b785bce85e07ef19091625077f629764afe6bf8
golamrabbani93 <golamrabbani9373@gmail.com>
2023-08-29 21:40:43 +0600

    navbar fixed

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
 		</>
 	);
 	return (
-		<div className="navbar bg-base-100 lg:w-[1440px]">
+		<div className="navbar bg-base-100 flex justify-between m-auto lg:w-[1440px]">
 			<div className="navbar-start flex flex-row-reverse justify-end">
 				<div className="dropdown ">
 					<label tabIndex={0} className="btn btn-ghost lg:hidden">

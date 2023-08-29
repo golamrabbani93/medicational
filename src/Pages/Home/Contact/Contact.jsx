ce5226c20d3b186bf3eb0f5eea608c1b9e6298a7
golamrabbani93 <golamrabbani9373@gmail.com>
2023-08-29 21:26:22 +0600

    contact section responsive complete

--- src/Pages/Home/Contact/Contact.jsx
+++ src/Pages/Home/Contact/Contact.jsx
@@ -12,27 +12,30 @@ const Contact = () => {
 				<h3 className="text-secondary font-bold mb-2">Contact Us </h3>
 				<h2 className="text-white font-normal text-4xl mb-14">Stay connected with us</h2>
 			</div>
-			<div className="contact-form w-[450px] m-auto">
+			<div className="contact-form w-96 md:w-[450px] m-auto">
 				<div className="mb-5">
 					<input
 						type="text"
-						placeholder="Type here"
+						placeholder="Email"
 						className="input input-bordered input-secondary w-full "
 					/>
 				</div>
 				<div className="mb-5">
 					<input
 						type="text"
-						placeholder="Type here"
+						placeholder="Subject"
 						className="input input-bordered input-secondary w-full "
 					/>
 				</div>
 				<div className="mb-5">
-					<textarea className="textarea textarea-primary w-full  h-36" placeholder="Bio"></textarea>
+					<textarea
+						className="textarea textarea-primary w-full  h-36"
+						placeholder="Your Message"
+					></textarea>
 				</div>
 
-				<button className="btn btn-primary bg-gradient-to-r from-secondary to-primary">
-					Get Started
+				<button className="btn btn-primary text-white font-bold bg-gradient-to-r from-secondary to-primary">
+					Submit
 				</button>
 			</div>
 		</div>

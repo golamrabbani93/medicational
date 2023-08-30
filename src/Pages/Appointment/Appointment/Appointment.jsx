5a731b14bd22afa1f8e35fbedbcdc99ff6705d32
golamrabbani93 <golamrabbani9373@gmail.com>
2023-08-30 16:15:44 +0600

    create apponitment page

---
+++ src/Pages/Appointment/Appointment/Appointment.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Appointment = () => {
+	return (
+		<div>
+			<h2>thi is a appontment page</h2>
+		</div>
+	);
+};
+
+export default Appointment;
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -2,6 +2,7 @@ import {createBrowserRouter} from 'react-router-dom';
 import Home from '../../Pages/Home/Home/Home';
 import Login from '../../Pages/Resgister/Login/Login';
 import Main from '../../layouts/Main/Main';
+import Appointment from '../../Pages/Appointment/Appointment/Appointment';
 
 const router = createBrowserRouter([
 	{
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
 			{
 				path: '/',
 				element: <Home></Home>,
+			},
+			{
+				path: '/appointment',
+				element: <Appointment></Appointment>,
 			},
 			{
 				path: '/login',

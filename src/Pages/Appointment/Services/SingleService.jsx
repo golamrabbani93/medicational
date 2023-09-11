1bb24904714e36b839656a39770c2fd31b5e5d09
golamrabbani93 <golamrabbani9373@gmail.com>
2023-09-11 16:46:37 +0600

    add some condition

--- src/Pages/Appointment/Services/SingleService.jsx
+++ src/Pages/Appointment/Services/SingleService.jsx
@@ -1,4 +1,3 @@
-import {format} from 'date-fns';
 import React from 'react';
 
 const SingleService = ({service, date}) => {
@@ -7,10 +6,15 @@ const SingleService = ({service, date}) => {
 		<div className="card w-96 bg-base-100 shadow-xl">
 			<div className="card-body text-center">
 				<h2 className="text-secondary text-xl font-semibold">{name}</h2>
-				<p> {format(date, 'PP')}</p>
-				<p>{slots.length > 0 ? `${slots.length} SPACES AVAILABLE` : 'Try another day'}</p>
+				<p>{slots.length > 0 ? slots[0] : 'Try Another day'}</p>
+				<p>
+					{slots.length} {slots.length > 1 ? 'SPACES' : 'SPACE'} AVAILABLE
+				</p>
 				<div className="card-actions justify-center">
-					<button className="btn btn-primary bg-gradient-to-r from-secondary to-primary">
+					<button
+						disabled={slots.length === 0}
+						className="btn btn-primary bg-gradient-to-r from-secondary to-primary text-white"
+					>
 						Book Appointment
 					</button>
 				</div>

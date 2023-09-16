324c18b71db9d8cfca076cec5047d88dc9ae63cc
golamrabbani93 <golamrabbani9373@gmail.com>
2023-09-16 20:35:07 +0600

    dashboard drawer button fixed

--- src/layouts/DashBoardLayout/DashBoardLayout.jsx
+++ src/layouts/DashBoardLayout/DashBoardLayout.jsx
@@ -16,15 +16,30 @@ const DashBoardLayout = () => {
 		<div>
 			<Navbar></Navbar>
 			<div className="drawer lg:drawer-open">
-				<input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+				<input id="dashboard-drawer" type="checkbox" className="drawer-toggle" />
 				<div className="drawer-content">
-					<Outlet></Outlet>
-					<label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">
-						Open drawer
+					<label htmlFor="dashboard-drawer" tabIndex={0} className="btn btn-ghost lg:hidden">
+						<svg
+							width="20"
+							height="20"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							className="inline-block h-5 w-5 stroke-current md:h-6 md:w-6"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								d="M4 6h16M4 12h16M4 18h16"
+							></path>
+						</svg>
 					</label>
+
+					<Outlet></Outlet>
 				</div>
 				<div className="drawer-side">
-					<label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+					<label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
 					<ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
 						{/* Sidebar content here */}
 						{dashboardLink}

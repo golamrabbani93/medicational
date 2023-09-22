b77d22a7a498456db987dc00fe27aa14f64020a5
golamrabbani93 <golamrabbani9373@gmail.com>
2023-09-23 01:57:12 +0600

    Remove .evv.local from tracking

--- src/hooks/UseAdmin.js
+++ src/hooks/UseAdmin.js
@@ -1,16 +1,21 @@
 import {useEffect, useState} from 'react';
 
 const UseAdmin = (email) => {
-	const [admin, setAdmin] = useState('');
+	const [admin, setAdmin] = useState(false);
 	const [adminLoading, setAdminLoading] = useState(true);
 	useEffect(() => {
 		if (email) {
-			fetch(`http://localhost:5000/users/admin/${email}`)
-				.then((res) => res.json())
-				.then((data) => {
+			async function fetchAdmin() {
+				try {
+					const res = await fetch(`http://localhost:5000/users/admin/${email}`);
+					const data = await res.json();
 					setAdmin(data.Admin);
 					setAdminLoading(false);
-				});
+				} catch (error) {
+					console.log(error, 'C');
+				}
+			}
+			fetchAdmin();
 		}
 	}, [email]);
 	return [admin, adminLoading];

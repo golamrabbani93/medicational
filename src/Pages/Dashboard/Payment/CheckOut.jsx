f19798ce6c0d4c0bfc5105ba7de52e27ff7bac4a
golamrabbani93 <golamrabbani9373@gmail.com>
2023-09-25 10:40:56 +0600

    Add stripe Payment

---
+++ src/Pages/Dashboard/Payment/CheckOut.jsx
@@ -0,0 +1,110 @@
+import {CardElement, useElements, useStripe} from '@stripe/react-stripe-js';
+import React, {useEffect} from 'react';
+import {useState} from 'react';
+const CheckOut = ({booking}) => {
+	const {name, email} = booking;
+	const stripe = useStripe();
+	const elements = useElements();
+	// !Card Error Message
+	const [cardError, SetCardError] = useState('');
+	const [clientSecret, setClientSecret] = useState('');
+	const [success, setSuccess] = useState('');
+	const [processing, setProcessing] = useState(false);
+	useEffect(() => {
+		// Create PaymentIntent as soon as the page loads
+		fetch('http://localhost:5000/payment/create-payment-intent', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(booking),
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				setClientSecret(data.clientSecret);
+			});
+	}, [booking]);
+
+	// !get card data
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+
+		if (!stripe || !elements) {
+			return;
+		}
+		const card = elements.getElement(CardElement);
+
+		if (card == null) {
+			return;
+		}
+		const {error, paymentMethod} = await stripe.createPaymentMethod({
+			type: 'card',
+			card,
+		});
+
+		if (error) {
+			SetCardError(error.message);
+		} else {
+			SetCardError('');
+			console.log('[PaymentMethod]', paymentMethod);
+		}
+		setProcessing(true);
+		const {paymentIntent, error: confirmError} = await stripe.confirmCardPayment(clientSecret, {
+			payment_method: {
+				card: card,
+				billing_details: {
+					name: name,
+					email: email,
+				},
+			},
+		});
+		if (confirmError) {
+			SetCardError(confirmError.message);
+			return;
+		}
+		if (paymentIntent.status === 'succeeded') {
+			SetCardError('');
+			setSuccess('Congrats! your payment completed');
+		}
+		elements.getElement(CardElement).clear();
+		setProcessing(false);
+	};
+	return (
+		<div>
+			{' '}
+			<form onSubmit={handleSubmit}>
+				<CardElement
+					options={{
+						style: {
+							base: {
+								fontSize: '16px',
+								color: '#424770',
+								'::placeholder': {
+									color: '#aab7c4',
+								},
+							},
+							invalid: {
+								color: '#9e2146',
+							},
+						},
+					}}
+				/>
+				<button
+					className="btn btn-sm btn-secondary mt-2"
+					type="submit"
+					disabled={!stripe || !clientSecret || processing}
+				>
+					Pay
+				</button>
+			</form>
+			{cardError && <p className="text-red-500">{cardError}</p>}
+			{success && (
+				<div>
+					<p className="text-green-500">{success}</p>
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default CheckOut;
--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,13 +1,56 @@
 import React from 'react';
-import {useParams} from 'react-router-dom';
-
+import {useContext} from 'react';
+import toast from 'react-hot-toast';
+import {useQuery} from 'react-query';
+import {Navigate, useLocation, useParams} from 'react-router-dom';
+import {AuthContext} from '../../../Contexts/AuthProvider';
+import {Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
+import CheckOut from './CheckOut';
+const stripePromise = loadStripe(
+	'pk_test_51NtpOFBOLaOXSm4LuS89Q8odYmfFTfHgAl4ArobAyr5ttT58LsnRgXiyhU7g3KRnVGoEvo0LWudjUl6VGq3Yh2WZ00ku7FCX07',
+);
 const Payment = () => {
-	const bookingId = useParams();
-	console.log('🚀🚀: Payment -> bookingId', bookingId);
+	const {logOut} = useContext(AuthContext);
+	const {id} = useParams();
+	const location = useLocation();
+	const bookingURL = `http://localhost:5000/booking/${id}`;
+
+	// !get booking by querry
+	const {data: booking = []} = useQuery({
+		queryKey: ['booking', id],
+		queryFn: async () => {
+			const res = await fetch(bookingURL, {
+				headers: {
+					authorization: `bearer ${localStorage.getItem('Token')}`,
+				},
+			});
+			//! Token Expired
+			if (res.status === 403) {
+				toast.error('Token Expired');
+				logOut();
+				<Navigate to="/login" state={{from: location}} replace />;
+			}
+			const data = await res.json();
+			return data.data;
+		},
+	});
+
 	return (
 		<div>
 			<div>
-				<h3 className="text-2xl mb-6">Payment</h3>
+				<h3 className="text-3xl mb-6">Payment</h3>
+			</div>
+			<div>
+				<h3 className="text-2xl">Booking Information</h3>
+				<p>Treatment Name: {booking?.treatment}</p>
+				<p>Treatment Date: {booking?.date}</p>
+				<p>Treatment Price: {booking?.price}</p>
+			</div>
+			<div className="w-96 mt-7">
+				<Elements stripe={stripePromise}>
+					<CheckOut booking={booking} />
+				</Elements>
 			</div>
 		</div>
 	);
